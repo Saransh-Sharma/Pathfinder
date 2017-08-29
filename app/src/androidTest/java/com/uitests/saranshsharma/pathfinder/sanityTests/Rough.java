ce04ec3948103669de2f5f6592fc08fe7c9c13ae
Saransh Sharma <saransh1337@gmail.com>
2017-08-29 17:58:39 +0530

    adds pathfinder project stub

---
+++ app/src/androidTest/java/com/uitests/saranshsharma/pathfinder/sanityTests/Rough.java
@@ -0,0 +1,12 @@
+package com.uitests.saranshsharma.pathfinder.sanityTests;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by saranshsharma on 8/29/17.
+ */
+
+public class Rough extends InstrumentationTestCase {
+
+
+}
